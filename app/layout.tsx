98bf6bf1af3c586aaac0e12a78df3702d6cf62d2
aeholic <mkd6lic0@hotmail.de>
2023-06-01 01:58:37 +0200

    schedule updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@
 
 import './globals.css'
 import Image from 'next/image'
+import Link from 'next/link'
 import logo from '§/public/aespa_logo.png'
 import { Noto_Sans_KR } from 'next/font/google'
 import { Oswald } from 'next/font/google'
@@ -10,7 +11,11 @@ import type { Children } from '§/types'
 import KoreaTime from '§/components/KoreaTime'
 import Project from '§/lib/utils'
 
-const osw = Oswald({ subsets: ['latin'] })
+const osw = Oswald({ 
+	subsets: ['latin'], 
+	style: 'normal',
+	weight: ['200', '400', '500', '700']
+})
 const noto = Noto_Sans_KR({
   subsets: ['latin'],
   weight: ['100', '400', '500', '700'],
@@ -28,19 +33,19 @@ export default function RootLayout({ children }: Children) {
 			<body>
 				<header className={`${osw.className}`}>
 					<div className="flex p-5">
-						<a href="/" className="flex title-font font-medium items-center text-white">
+						<Link href="/" className="flex title-font font-medium items-center text-white">
 							<Image id="logo" src={logo} alt="aespa Fancams" title="aespa Fancams" />
 							<span className="ml-3 mr-8 text-2xl">aespacore</span>
-						</a>
+						</Link>
 						<nav>
 							<div id="topnavi">
 								<div className="menu">
-									<span><a href="/">Home</a></span>
-									<span><a href="/event-schedule">Event Schedule</a></span>
-									<span><a href="/achievements">Achievements</a></span>
-									<span><a href="/content-timeline">Content Timeline</a></span>
-									<span><a href="/fancams">Fancams</a></span>
-									<span><a href="/brand-reputation">Brand Reputation</a></span>
+									<span><Link href="/">Home</Link></span>
+									<span><Link href="/event-schedule">Event Schedule</Link></span>
+									<span><Link href="/achievements">Achievements</Link></span>
+									<span><Link href="/content-timeline">Content Timeline</Link></span>
+									<span><Link href="/fancams">Fancams</Link></span>
+									<span><Link href="/brand-reputation">Brand Reputation</Link></span>
 								</div>
 								<div className="menu-right">
                   <KoreaTime />
--- components/KoreaTime.tsx
+++ components/KoreaTime.tsx
@@ -7,17 +7,17 @@ import { useState, useEffect } from "react"
 
 const KoreaTime = () : JSX.Element => {
   
-  let kst = '00:00:00am 00/00/00'
+  let kst = '00:00:00 00/00/00'
   const [currentTime, setCurrentTime] = useState(kst)
 
   useEffect(() => {
     setInterval(() => {
-      let kst = moment().utcOffset(540).format('hh:mm:ssa YY/MM/DD')
+      let kst = moment().utcOffset(540).format('hh:mm:ss YY/MM/DD')
       setCurrentTime(kst)
     }, 1000)
   }, [])
 
-  return <span className="koreatime self-end">Korea Time: {currentTime}</span>
+  return <span className="koreatime self-end">KST {currentTime}</span>
 }
 
 export default KoreaTime
