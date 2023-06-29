01acb3594e87b14b91fc2db48cb48224c3c48714
aeholic <mkd6lic0@hotmail.de>
2023-06-29 02:47:31 +0200

    memory game updated

--- components/MemoryGame.tsx
+++ components/MemoryGame.tsx
@@ -16,8 +16,7 @@ import _ from 'underscore'
 
 const MemoryGame = (): JSX.Element => {
 
-  let crd1 = 0
-  let crd2 = 0
+  let [crd1, crd2] = [0, 0]
   
   const 
     [table, setTable] = useState<any>(),
@@ -34,14 +33,20 @@ const MemoryGame = (): JSX.Element => {
       {id: 5, match: 1, face: 'card bg-red-500'}, {id: 6, match: 2, face: 'card bg-blue-500'}, {id: 7, match: 3, face: 'card bg-yellow-500'}, {id: 8, match: 4, face: 'card bg-green-500'} 
       // {id: '5-1', face: 'card bg-orange-500'}, {id: '6-1', face: 'card bg-pink-500'}, {id: '7-1', face: 'card bg-cyan-500'}, {id: '8-1', face: 'card bg-purple-500'},
     ]),
+
     
     // [firstID, setFirstID] = useState<string | null>(null),
     // [firstID, setFirstID] = useState<string | null>(null),
-    [card2, setCard2] = useState(),
     // card1 = useRef('')
-    [card1, setCard1] = useState(),
+    [card2, setCard2] = useState<any>(),
+    [card1, setCard1] = useState<any>(),
+    [match, setMatch] = useState<any>(),
+    [removedCards, setRemovedCards] = useState<any[]>([1]),
+    [deck, setDeck] = useState<any>([]),
     faceRef: any | null | undefined = useRef<HTMLDivElement>(null)
     
+    // console.log(cards.filter(c=>c.match !== removeCard))
+    // console.log(cards.filter((c:any)=> !removedCards.includes(c.match)))
   // const checkSameCard = (face?: any) => {
   //   if (!firstID) {
   //     setFirstID(face.id)
@@ -64,22 +69,23 @@ const MemoryGame = (): JSX.Element => {
       crd1 = card
       console.log(crd1)
     } else if (crd1 !== 0) {
-      crd2 = card
       console.log(crd1)
-      if (crd1 === crd2) {
+      if (crd1 === card) {
+        setMatch('***** MATCH! *****')
         console.log('***** MATCH! *****')
+        setRemovedCards((rest) => [...rest, crd1])
+        setDeck(deck.filter((c:any)=> !removedCards.includes(c.match)))
       }
-      crd1 = 0
-      crd2 = 0
+      // crd1 = 0
+      // crd2 = 1
     }
   }
 
   const randomCards: VoidFunction = () => {
-    let i: number = 0
-    const random: Array<string> = _.shuffle(cards)
+    setDeck(_.shuffle(cards))
     setTable(
       // random.map((color: string): JSX.Element => <div key={i++} {...{className: `card bg-${color}-500`}}></div>)
-      random.map((color: any): JSX.Element => <div key={i++} {...{className: `covered ${color.face}`}}
+      deck.map((color: any): JSX.Element => <div key={color.id} {...{className: `covered ${color.face}`}}
         ref={faceRef} data-face={color.match} onClick={()=>setCard(color.match)}>
       </div>)
     )
@@ -88,7 +94,6 @@ const MemoryGame = (): JSX.Element => {
     
   useEffect(()=> randomCards(), [])
   useEffect(()=> {
-    crd1 = 1 
     // console.log('Data :'+faceRef.current?.getAttribute('data-face'))
     // if (card1 !== '') {
     //   setCard2(card1)
@@ -97,8 +102,13 @@ const MemoryGame = (): JSX.Element => {
       // console.log(faceRef.current?.getAttribute('data-face'))
     // }
     // else setCard1(card2)
-    console.log('effect: '+crd2) 
-    console.log('effect: '+crd1) 
+    setCard1(crd1)
+    setCard2(crd2)
+    console.log('effect: '+crd2)
+    console.log('effect: '+crd1)
+    setTable(deck.map((color: any): JSX.Element => <div key={color.id} {...{className: `covered ${color.face}`}}
+      ref={faceRef} data-face={color.match} onClick={()=>setCard(color.match)}>
+    </div>))
     // if (card2 && card1) return
     // else if (card2 === card1) {
     //   console.log('MATCH!') 
@@ -124,14 +134,14 @@ const MemoryGame = (): JSX.Element => {
     //   setSecondID('')
     //   console.log('reseted.')
     // }
-  }, [])
+  }, [card1, card2, removedCards, deck])
 
   return (
     <>
       <button className="border" onClick={randomCards}>Refresh</button><br />
       1st Card: {card1}<br />
-      2nd Card: {card2}
-      {card1 === card2 ? 'MATCH!' : ''}
+      2nd Card: {card2}<br />
+      {card1 === card2 ? match : ''}
       <div className="memory">
         {table}
       </div>
