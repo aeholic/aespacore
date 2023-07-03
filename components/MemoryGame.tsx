ed25bd07f25978ee0b0b5fcf7bd3fe23e1b811b2
aeholic <mkd6lic0@hotmail.de>
2023-07-03 21:46:44 +0200

    memory game updated

--- components/MemoryGame.tsx
+++ components/MemoryGame.tsx
@@ -2,43 +2,61 @@
 
 'use client'
 
-import React, { Dispatch, EventHandler } from 'react'
 import { useState, useEffect } from 'react'
 import Image from 'next/image'
 import logo from '§/public/aespa_logo.png'
 import _ from 'underscore'
 
-let 
-	firstCard: any = {
-		id: 0,
-		match: 0
-	},
-	solvedCards: number[] = []
+interface CardClickProps {
+	(currentCard: CardProps) : void
+}
 
-const MemoryGame = (): JSX.Element => {
+type CardProps = {
+	id: number
+	match: number
+	covered: boolean
+	face: string
+}
+type CheckedCardsProps = {
+	id: number
+	match: number 
+	solved: Array<number>
+}
+
+let checkedCards: CheckedCardsProps = {
+	id: 0,
+	match: 0,
+	solved: []
+}
+
+const 
+	cards: Array<CardProps> = [],
 	
-	const 
-		cards: any[] = [
-			{id: 1, match: 1, face: `bg-[url(/memory/ningning1.png)]`}, {id: 2, match: 2, face: `bg-[url(/memory/karina1.png)]`},
-			{id: 5, match: 1, face: `bg-[url(/memory/ningning1.png)]`}, {id: 6, match: 2, face: `bg-[url(/memory/karina1.png)]`},
-			
-			{id: 3, match: 3, face: `bg-[url(/memory/winter1.png)]`}, {id: 4, match: 4, face: `bg-[url(/memory/giselle1.png)]`},
-			{id: 7, match: 3, face: `bg-[url(/memory/winter1.png)]`}, {id: 8, match: 4, face: `bg-[url(/memory/giselle1.png)]`},
-			
-			// {id: 9, match: 5, face: 'bg-teal-500'}, {id: 10, match: 6, face: 'bg-pink-500'},
-			// {id: 13, match: 5, face: 'bg-teal-500'}, {id: 14, match: 6, face: 'bg-pink-500'},
-		
-			// {id: 11, match: 7, face: 'bg-orange-500'}, {id: 12, match: 8, face: 'bg-purple-500'},
-			// {id: 15, match: 7, face: 'bg-orange-500'}, {id: 16, match: 8, face: 'bg-purple-500'},
-			
-			// {id: 17, match: 9, face: 'bg-cyan-500'},
-			// {id: 18, match: 9, face: 'bg-cyan-500'}
-		],
-		[table, setTable] = useState<any>(),
-		[deck, setDeck] = useState<any>(_.shuffle(cards)),
-		[cardIndex, setCardIndex] = useState<any>(null)
+	addCards = (newFace: Array<string>): void => {
+		const abc = (i: number, a: number): CardProps => {
+			return { id: i++, match: a + 1, covered: true, face: newFace[a]}
+		}
+		for (let i = 1; i <= newFace.length; i++) {
+			for (let a = 0; a < newFace.length; a++) {
+				cards.push(
+					abc(i++, a), abc(i++, a)
+				)
+			}
+		}
+	}
 
-	const handleCardClick = (match: number, id: number) => {
+addCards([
+	'bg-[url(/memory/k1.png)]', 'bg-[url(/memory/w1.png)]', 'bg-[url(/memory/n1.png)]', 'bg-[url(/memory/g1.png)]',
+	'bg-[url(/memory/k2.png)]', 'bg-[url(/memory/w2.png)]', 'bg-[url(/memory/n2.gif)]', 'bg-[url(/memory/g2.gif)]'
+])
+
+const MemoryGame: React.FC = (): JSX.Element => {
+	
+	const
+		[table, setTable] = useState<Array<JSX.Element>>(),
+		[deck, setDeck] = useState<Array<CardProps>>(_.shuffle(cards))
+		
+	const handleCardClick: CardClickProps = currentCard => {
 		let settings = {
 			delay: 500,
 			flipped: ' flipped',
@@ -48,70 +66,63 @@ const MemoryGame = (): JSX.Element => {
 			disable: ' pointer-events-none'
 		}
 
-		if (firstCard.match !== 0) {
-
-			if (firstCard.match !== match) {
+		if (checkedCards.match !== 0) {
+			if (checkedCards.match !== currentCard.match) {
 				setTimeout(()=> {
-					const flipDeck = deck.filter((v:any) => v.face = 
-						v.face
-							.replace(settings.flipped, '')
-							.replace(settings.disable, ''))
+					const flipDeck = deck.filter((v: CardProps): Array<string | boolean> => {
+						return [
+							v.face = v.face.replace(settings.flipped, '').replace(settings.disable, ''),
+							v.covered = true
+						]
+					})
 
 					setDeck(flipDeck)
 				}, settings.delay)
 
-				firstCard.match = 0
+				checkedCards.match = 0
 			} else {
-				solvedCards.push(firstCard.match)
+				checkedCards.solved.push(checkedCards.match)
 
 				setTimeout(()=> {
-					const newDeck = deck.filter((v:any) => v.face = 
-						solvedCards.includes(v.match) ? 
-							v.face+settings.hide+settings.show : v.face)
+					const newDeck = deck.filter((v: CardProps): string => {
+						if (checkedCards.solved.includes(v.match)) {
+							return v.face = v.face+settings.hide+settings.show
+						 } else {
+							return v.face
+						 }
+					})
 
 					setDeck(newDeck)
 				}, settings.delay)
 
-				firstCard.match = 0
+				checkedCards.match = 0
 			}
 		} else {
-			firstCard.match = match
-			firstCard.id = id
+			checkedCards.match = currentCard.match
+			checkedCards.id = currentCard.id
 		}
 
-		const flipped = deck.filter((v:any) => v.face = 
-			id === v.id ? 
-				v.face+settings.flipped+settings.disable : v.face)
+		const flipped = deck.filter((v: CardProps): Array<string | boolean> | string => {
+			if (currentCard.id === v.id) {
+				return [
+					v.face = v.face+settings.flipped+settings.disable,
+					v.covered = false
+				]
+			} else {
+				return v.face
+			}
+		})
 		
 		setDeck(flipped)
 	}
 
-	// handleCardClick.getCard = (c: any) => {
-	// 	if (cardIndex === c.id) {
-	// 		return true
-	// 	} else {
-	// 		cards.filter((v:any) => {
-	// 			v.match === c.match 
-	// 		})
-	// 	}
-	// }
-
-	const randomCards: VoidFunction = () => {
+	const randomCards: VoidFunction = (): void => {
 		setTable(
 			deck.map(
 				(c: any): JSX.Element => (
 					<div key={c.id} className="card-wrap">
-						<div
-							key={c.id} 
-							className={`card ${c.face}`}
-							onClick={() => handleCardClick(c.match, c.id)}
-						>
-							{/* <Image src={logo} alt="" /> */}
-							{/* <img 
-								onClick={()=>setCardIndex(c.id)} 
-								className={`default-deck ${handleCardClick.getCard(c) ? 'opacity-0' : ''}`} 
-								src="/aespa_logo.png"
-							/> */}
+						<div key={c.id} className={`card ${c.face}`} onClick={() => handleCardClick(c)}>
+							<Image alt='aespa_logo.png' className={`default-deck ${c.covered ? 'opacity-100' : 'opacity-0'}`} src={logo} priority={true}/>
 						</div>
 					</div>
 				)
@@ -119,15 +130,9 @@ const MemoryGame = (): JSX.Element => {
 		)
 	}
 
-	useEffect(() => {
-		randomCards()
-	}, [deck])
+	useEffect(() => randomCards(), [deck])
 
-	return (
-		<div className='memory'>
-			{table}
-		</div>
-	)
+	return <div className='memory'>{table}</div>
 }
 
 export default MemoryGame
