15113b8e764c95095ed8004f984dbf3bc210ec3c
aeholic <mkd6lic0@hotmail.de>
2023-07-15 02:30:53 +0200

    continual events updated

--- app/event-schedule/page.tsx
+++ app/event-schedule/page.tsx
@@ -5,11 +5,47 @@ import Utils from '§/lib/utils'
 import Event from '§/components/Event'
 import type { EventProps } from '§/types/types'
 import type { EventApiResponse } from '../api/events/route'
+import dayjs from 'dayjs'
+import utc from 'dayjs/plugin/utc'
 
 export const metadata: Metadata = { 
   title: `${Utils.Project} - Event Schedule`,
   description: 'Event schedule.'
 }
+
+dayjs.extend(utc)
+
+type continualProps = {
+  status: number
+  originalDate: string
+}
+
+type continualFuncs = {
+  nextDate?: string
+  time?: string
+  daily?: (props: continualProps) => string | undefined
+  weekly?: (props: continualProps) => string | (string | undefined)[]
+}
+
+const continual: continualFuncs = {}
+
+continual.nextDate = '2023-07-16'
+continual.time = '09:30:00'
+
+console.log('Current DateTime: '+dayjs().utcOffset(540).format('YYYY-MM-DD HH:mm:ss'))
+console.log('MelOn DateTime: '+dayjs(`${continual.nextDate} ${continual.time}`).format('YYYY-MM-DD HH:mm:ss'))
+const curDate = dayjs(`${continual.nextDate} ${continual.time}`).utcOffset(540)
+const dateDiff = curDate.diff(dayjs().utcOffset(540), 'hours')
+console.log('MelOn Difference: '+dateDiff)
+
+
+continual.daily = (props: continualProps) => (
+  props.status === 5 ? continual.nextDate : props.originalDate
+)
+
+// continual.weekly = (props: continualProps) => (
+//   props.status === 5 ? [props.fixedDate, props.nextDate] : props.originalDate
+// )
 
 const 
   getEvents = async (): Promise<any> => {
@@ -22,6 +58,8 @@ const
   addEvent = async (data: EventProps): Promise<any> => {},
   editEvent = async (data: EventProps): Promise<any> => {},
   deleteEvent = async (data: EventProps): Promise<any> => {}
+
+
 
 export default async function EventSchedulePage() : Promise<JSX.Element> {
 
@@ -43,20 +81,27 @@ export default async function EventSchedulePage() : Promise<JSX.Element> {
           </div>
           {
             events.sort((a: any, b: any) => (
-              a.status === 5 ? '2023-07-17' : a.date) + a.time > ( b.status === 5 ? '2023-07-17' : b.date) + b.time ? 1 : -1
-              ).map((event: any) => {
-                event.date = event.status === 5 ? '2023-07-17' : event.date
-                return <Event key={event.id}
-                  dateTime={`${event.date} ${event.time}`}
-                  eventName={event.eventName}
-                  confirmed={event.confirmed}
-                  category={event.category}
-                  link={event.link}
-                  image={event.image}
-                  status={event.status}
-                  reminder={event.reminder}
-                  id={event.id}
-                />
+              (continual.daily && continual.daily({
+                status: a.status, originalDate: a.date
+              })) + a.time > 
+              (continual.daily && continual.daily({ 
+                status: b.status, originalDate: b.date
+              })) + b.time ? 1 : -1
+            )).map((event: any) => {
+              event.date = (continual.daily && continual.daily({
+                status: event.status, originalDate: event.date
+              }))
+              return <Event key={event.id}
+                dateTime={`${event.date} ${event.time}`}
+                eventName={event.eventName}
+                confirmed={event.confirmed}
+                category={event.category}
+                link={event.link}
+                image={event.image}
+                status={event.status}
+                reminder={event.reminder}
+                id={event.id}
+              />
             })
           }
         </div>
